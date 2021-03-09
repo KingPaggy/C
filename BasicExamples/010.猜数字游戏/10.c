6ea671fa2a64c27947ac599e412e979136b74655
PageKim <kingpage12138@qq.com>
2021-03-09 10:56:02 +0800

    test and modify 010

--- BasicExamples/010.猜数字游戏/10.c
+++ BasicExamples/010.猜数字游戏/10.c
@@ -1,59 +1,45 @@
 #include <stdio.h>
-#include <conio.h>
-void main()
+#include <stdlib.h>
+#include <time.h>
+/*
+time()   defined in header <time.h>
+rand()   defined in header <stdlib.h>, returns a pseudo-random integer value between [0,RAND_MAX].
+srand()  defined in header <stdlib.h>, seeds the pseudo-random number generator used by rand() with the value seed.
+RAND_MAX defined in header <stdlib.h>
+ */
+int main(void)
 {
-	int Password=0,Number=0,price=58,i=0;
-	clrscr();
-	printf("\n====This is a Number Guess Game!====\n");
-	while( Password != 1234 )
-	{
-		if( i >= 3 )
-			return;
-		i++;
-		puts("Please input Password: ");
-		scanf("%d",&Password);
+	int Number=0;
+
+	srand(time(NULL)); // Use current time as seed for random generator
+	
+	int price = rand()/(RAND_MAX/100);
+
+	if( price >100 || price <0){
+		puts("Error: the price generated is not between [0,100].");
+		return 0;
 	}
 
-	i=0;
+	printf("\n====This is a Number Guess Game!====\n");
+
+	// Guess the number
 	while( Number!=price )
 	{
 		do{
 			puts("Please input a number between 1 and 100: ");
 			scanf("%d",&Number);
-			printf("Your input number is %d\n",Number);
 		}while( !(Number>=1 && Number<=100) );
-		if( Number >= 90 )
-		{
-			printf("Too Bigger! Press any key to try again!\n");
-		}
-		else if( Number >= 70 && Number < 90 )
-		{
-			printf("Bigger!\n");
-		}
-		else if( Number >= 1 && Number <= 30 )
-		{
-			printf("Too Small! Press any key to try again!\n");
-		}
-		else if( Number > 30 && Number <= 50 )
-		{
-			printf("Small! Press any key to try again!\n");
-		}
-		else
-		{
-			if( Number == price )
-			{
-				printf("OK! You are right! Bye Bye!\n");
-			}
-			else if( Number < price )
-			{
-				printf("Sorry,Only a little smaller! Press any key to try again!\n");
 
-			}
-			else if( Number > price )
-				printf(" Sorry, Only a little bigger! Press any key to try again!\n");
+		if( Number == price ){
+			printf("\nYou are right! Bye Bye!\n");
 		}
-		getch();
+		else if( Number < price ){
+			printf("Smaller!\n\n");
+		}
+		else if( Number > price )
+			printf("Bigger!\n\n");
+
 	}
+	return 0;
 }
 
-
